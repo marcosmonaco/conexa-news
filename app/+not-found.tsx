72f8fb1bd72f9d86247236c6340fd573eab23d30
marcosmonaco <114680984+marcosmonaco@users.noreply.github.com>
2024-12-01 21:46:22 -0300

    Borrado de componentes prederteminados, cambios a la pagina notFound

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,8 +1,8 @@
 import {useTranslation} from "react-i18next";
 import {Link, Stack} from "expo-router";
 
-import {ThemedText} from "@/components/ThemedText";
-import {ThemedView} from "@/components/ThemedView";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Text, View} from "react-native";
 
 export default function NotFoundScreen() {
   const {t} = useTranslation();
@@ -10,12 +10,28 @@ export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{title: "Oops!"}} />
-      <ThemedView className="flex-1 items-center justify-center p-20">
-        <ThemedText type="title">{t("notFound.title")}</ThemedText>
-        <Link href="/(tabs)/posts" className="mt-12 py-12">
-          <ThemedText type="link">{t("notFound.goBack")}</ThemedText>
-        </Link>
-      </ThemedView>
+
+      <SafeAreaView className="flex-1 justify-center items-center bg-white">
+        <View className="flex flex-1 px-5 pt-20 gap-5">
+          <Text className="text-5xl font-bold text-center">
+            <Text className="text-blue-600">Conexa</Text> News
+          </Text>
+          <Text className="font-semibold text-xl text-center">
+            {t("notFound.title")}
+          </Text>
+
+          <View className="flex flex-col gap-8 ">
+            <Link
+              className="bg-blue-600 w-40 p-2 rounded-xl self-center"
+              href="/(tabs)/posts"
+            >
+              <Text className="text-center text-white">
+                {t("notFound.goBack")}
+              </Text>
+            </Link>
+          </View>
+        </View>
+      </SafeAreaView>
     </>
   );
 }
