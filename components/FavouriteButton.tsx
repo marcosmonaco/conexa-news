18baef79bf14a8da738855288decd4aa65482e26
marcosmonaco <114680984+marcosmonaco@users.noreply.github.com>
2024-12-01 21:28:33 -0300

    Testing de FavouriteButton

--- components/FavouriteButton.tsx
+++ components/FavouriteButton.tsx
@@ -1,27 +1,33 @@
 import React from "react";
-import {
-  GestureHandlerRootView,
-  TouchableOpacity,
-} from "react-native-gesture-handler";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 import {Ionicons} from "@expo/vector-icons";
 import {FavouriteButtonProps} from "@/models/favouriteButton";
+import {TouchableOpacity} from "react-native";
 
 export default function FavouriteButton({
   isFavourite: isFavorite,
   onPress,
 }: FavouriteButtonProps) {
+  const isFav = isFavorite ? "star" : "star-outline";
   return (
     <GestureHandlerRootView
       className={`absolute top-2 right-2 rounded-xl p-2 ${
         isFavorite ? "bg-yellow-600" : "bg-gray-500"
       }`}
+      testID="favourite-button"
     >
-      <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={onPress}
+        testID="favourite-wrapper"
+      >
         <Ionicons
-          name={isFavorite ? "star" : "star-outline"}
+          name={isFav}
           size={20}
           color="#FFFFFF"
+          accessibilityLabel={isFav}
+          testID="favourite-icon"
         />
       </TouchableOpacity>
     </GestureHandlerRootView>
---
+++ components/__tests__/FavouriteButton-test.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import {render, fireEvent} from "@testing-library/react-native";
+import FavouriteButton from "@/components/FavouriteButton";
+
+jest.mock("react-native-gesture-handler", () => ({
+  GestureHandlerRootView: require("react-native/Libraries/Components/View/View"),
+}));
+
+jest.mock("expo-font", () => ({
+  loadAsync: jest.fn(),
+  isLoaded: jest.fn().mockReturnValue(true),
+}));
+
+describe("FavouriteButton Component", () => {
+  it("Should render the button correctly with the favourite icon active", () => {
+    const {getByTestId} = render(
+      <FavouriteButton isFavourite={true} onPress={jest.fn()} />
+    );
+
+    const button = getByTestId("favourite-button");
+    const wrapper = getByTestId("favourite-wrapper");
+    const icon = getByTestId("favourite-icon");
+
+    // Verify that the button exists
+    expect(button).toBeTruthy();
+
+    // Verify that the touchableOpacity exists
+    expect(wrapper).toBeTruthy();
+
+    //Verify that it has the correct icon
+
+    expect(icon.props.accessibilityLabel).toEqual("star");
+  });
+
+  it("Should render the button correctly with the inactive favourite icon", () => {
+    const {getByTestId} = render(
+      <FavouriteButton isFavourite={false} onPress={jest.fn()} />
+    );
+
+    const button = getByTestId("favourite-button");
+    const wrapper = getByTestId("favourite-wrapper");
+
+    const icon = getByTestId("favourite-icon");
+
+    // Verify that the button exists
+    expect(button).toBeTruthy();
+
+    // Verify that the touchableOpacity exists
+    expect(wrapper).toBeTruthy();
+
+    //Verify that it has the correct icon
+    expect(icon.props.accessibilityLabel).toEqual("star-outline");
+  });
+
+  it("Should call the onPress function the button is pressed", () => {
+    const onPressMock = jest.fn();
+    const {getByTestId} = render(
+      <FavouriteButton isFavourite={true} onPress={onPressMock} />
+    );
+
+    const wrapper = getByTestId("favourite-wrapper");
+
+    // Simulates the event of pressing the button
+    fireEvent.press(wrapper);
+
+    // Verify that the onPress function was called
+    expect(onPressMock).toHaveBeenCalledTimes(1);
+  });
+});
